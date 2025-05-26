adab81c24156fe3b035e21d969086a621e179079
Rodolfo Kong <kongrodolfo@gmail.com>
2025-05-26 15:57:22 -0600

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,10 @@
-from . import fel_config
-from . import fel_document_type
-from . import fel_document
+# -*- coding: utf-8 -*-
+
 from . import fel_certification_provider
+from . import fel_document_type
+from . import fel_config
+from . import fel_document
+from . import fel_nit_verification
 from . import res_partner
 from . import account_move
 from . import pos_order
-from . import fel_nit_verification
