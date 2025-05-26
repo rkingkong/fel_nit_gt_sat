f7c5eedb39c6bc64a19c990d35f8763b1ec322c8
Rodolfo Kong <kongrodolfo@gmail.com>
2025-05-26 15:56:49 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,29 +1,43 @@
+# -*- coding: utf-8 -*-
+
 from . import models
 from . import wizard
 
 def post_init_hook(cr, registry):
-    """Post installation hook"""
-    # Create default FEL document types
-    env = registry['ir.environment']['res.users'].browse(cr, 1).env
+    """Post installation hook to setup default data"""
+    # Import here to avoid circular imports
+    env = registry['ir.environment'].with_context(active_test=False)
     
-    # Create INFILE provider
-    provider = env['fel.certification.provider'].get_infile_provider()
+    # Create default INFILE provider if it doesn't exist
+    provider_model = env['fel.certification.provider']
+    if not provider_model.search([('code', '=', 'infile')]):
+        provider_model.create({
+            'name': 'INFILE, S.A.',
+            'code': 'infile',
+            'website': 'https://www.infile.com',
+            'api_base_url': 'https://api.infile.com.gt',
+            'contact_email': 'zherrera@infile.com.gt',
+            'contact_phone': '2208-2208',
+            'supports_nit_verification': True,
+            'supports_xml_generation': True,
+            'supports_digital_signature': True,
+        })
     
-    # Create default document types
+    # Create default document types based on the INFILE proposal
     document_types = [
-        {'name': 'Factura', 'code': 'FACT', 'is_invoice': True},
-        {'name': 'Factura Cambiaria', 'code': 'FCAM', 'is_invoice': True},
-        {'name': 'Factura Pequeño Contribuyente', 'code': 'FPEQ', 'is_invoice': True},
-        {'name': 'Factura Cambiaria Pequeño Contribuyente', 'code': 'FCAP', 'is_invoice': True},
-        {'name': 'Factura Especial', 'code': 'FESP', 'is_invoice': True},
-        {'name': 'Nota de Abono', 'code': 'NABN'},
-        {'name': 'Recibo por Donación', 'code': 'RDON', 'is_receipt': True},
-        {'name': 'Recibo', 'code': 'RECI', 'is_receipt': True},
-        {'name': 'Nota de Débito', 'code': 'NDEB', 'is_debit_note': True},
-        {'name': 'Nota de Crédito', 'code': 'NCRE', 'is_credit_note': True},
+        {'name': 'Factura', 'code': 'FACT', 'is_invoice': True, 'available_for_general': True, 'available_for_pequeno': False, 'available_for_especial': True},
+        {'name': 'Factura Cambiaria', 'code': 'FCAM', 'is_invoice': True, 'available_for_general': True, 'available_for_pequeno': False, 'available_for_especial': True},
+        {'name': 'Factura Pequeño Contribuyente', 'code': 'FPEQ', 'is_invoice': True, 'available_for_general': False, 'available_for_pequeno': True, 'available_for_especial': False},
+        {'name': 'Factura Cambiaria Pequeño Contribuyente', 'code': 'FCAP', 'is_invoice': True, 'available_for_general': False, 'available_for_pequeno': True, 'available_for_especial': False},
+        {'name': 'Factura Especial', 'code': 'FESP', 'is_invoice': True, 'available_for_general': False, 'available_for_pequeno': False, 'available_for_especial': True},
+        {'name': 'Nota de Abono', 'code': 'NABN', 'is_credit_note': True, 'available_for_general': True, 'available_for_pequeno': True, 'available_for_especial': True},
+        {'name': 'Recibo por Donación', 'code': 'RDON', 'is_receipt': True, 'available_for_general': True, 'available_for_pequeno': True, 'available_for_especial': True},
+        {'name': 'Recibo', 'code': 'RECI', 'is_receipt': True, 'available_for_general': True, 'available_for_pequeno': True, 'available_for_especial': True},
+        {'name': 'Nota de Débito', 'code': 'NDEB', 'is_debit_note': True, 'available_for_general': True, 'available_for_pequeno': True, 'available_for_especial': True},
+        {'name': 'Nota de Crédito', 'code': 'NCRE', 'is_credit_note': True, 'available_for_general': True, 'available_for_pequeno': True, 'available_for_especial': True},
     ]
     
+    doc_type_model = env['fel.document.type']
     for doc_type_data in document_types:
-        existing = env['fel.document.type'].search([('code', '=', doc_type_data['code'])])
-        if not existing:
-            env['fel.document.type'].create(doc_type_data)
+        if not doc_type_model.search([('code', '=', doc_type_data['code'])]):
+            doc_type_model.create(doc_type_data)
