fba762e74d82b902dedf29b63f38ac9fb572fcf2
Rodolfo Kong <kongrodolfo@gmail.com>
2025-06-27 21:43:17 -0600

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,9 +2,11 @@
 
 from . import fel_certification_provider
 from . import fel_document_type
+from . import fel_tax_phrase  # ADD THIS LINE
 from . import fel_config
 from . import fel_document
 from . import fel_nit_verification
 from . import res_partner
 from . import account_move
 from . import pos_order
+from . import pos_config  # ADD THIS LINE
